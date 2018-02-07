8e47bf1c689c2b249e2b00fc57e0e81313928baa
Pozzi Luigi <luigi.pozzi@accenture.com>
2018-02-07 10:58:21 +0100

    Initial commit

---
+++ ProjectLime/src/main/java/com/example/appdiprova/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.appdiprova;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ActionMenuView;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button bt = (Button) findViewById(R.id.pulsante1);
+
+        //registra la callback dell'evento del click (codice eseguito al click)
+        bt.setOnClickListener(
+            new View.OnClickListener(){
+                @Override
+                public void onClick(View v){
+                    Intent ac = new Intent(getApplicationContext(), Main3Activity.class);
+                    startActivity(ac);
+                }
+            }
+        );
+
+    }
+
+}
